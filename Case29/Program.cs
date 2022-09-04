cfa5743fb2bb180cba088adcb9f3f81c2a4de938
unknown <Niromedium@gmail.com>
2022-09-04 22:43:12 +0500

    add Задача 29

---
+++ Case29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задает массив из 8 элементов и выводит их на экран
+void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index ++;
+    }
+}
+void PrintArray(int[]coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos ++;
+    }
+    Console.Write(" ]");
+}
+int []array = new int[8];
+FillArray(array);
+PrintArray(array);
