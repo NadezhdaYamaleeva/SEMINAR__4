4eda418d2a46d4f149f5f7682316c292f8e7e059
unknown <Niromedium@gmail.com>
2022-09-04 22:22:22 +0500

    add Задача 27

---
+++ Case27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимает на вход число и выдает сумму цифр в этом числе.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int count = 0;
+while (a != 0)
+{
+    int LastDig = a%10;
+    count = count + LastDig;
+    a = a/10;
+}
+Console.WriteLine("Сумма цифр в этом числе равна " + count);
