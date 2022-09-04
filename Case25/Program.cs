ed4e22659e15b293ab8914f3b4944f4b9d74f841
unknown <Niromedium@gmail.com>
2022-09-04 22:01:31 +0500

    add Задача 25

---
+++ Case25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int b = int.Parse(Console.ReadLine());
+int count = 1;
+for (int i = 1; i <= b; i ++)
+{
+    count = count*a;
+}
+Console.WriteLine(count);
